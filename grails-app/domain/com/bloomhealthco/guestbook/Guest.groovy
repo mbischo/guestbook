4343505085dce55491f023004c05f4fd13545a3b
Matt Bischoff <mbischoff@bloomhealthco.com>
2013-12-06 09:42:26 -0600

    Fixed date formatting, updated table style to look like existing app

--- grails-app/domain/com/bloomhealthco/guestbook/Guest.groovy
+++ grails-app/domain/com/bloomhealthco/guestbook/Guest.groovy
@@ -1,13 +1,18 @@
 package com.bloomhealthco.guestbook
 
 import grails.rest.*
+import org.grails.databinding.BindingFormat
 
 @Resource(uri='/guests', formats=['json'])
 class Guest {
 
     String name
     String company
+
+    @BindingFormat("yyyy-MM-dd'T'hh:mm:ss.S'Z'")
     Date checkinTime
+
+    @BindingFormat("yyyy-MM-dd'T'hh:mm:ss.S'Z'")
     Date checkoutTime
     Employee visitedEmployee
 
